91f4fb753ee72f9adfe26a240b6f405dc9430ef6
dawidskalka <jandrygasiewicz5@gmail.com>
2025-11-04 14:29:09 +0100

    zadania

---
+++ konwert/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace konwerter
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double temperaturaCelsjusz = 25;
+            double farenheit = (temperaturaCelsjusz * 9 / 5) + 32;
+            temperaturaCelsjusz = (farenheit - 32) * 5 / 9;
+            Console.WriteLine($"temperatura w farenheitach: {Math.Round(farenheit, 2)}");
+            Console.WriteLine($"Konwersjaz powrotem do celsjusza:{Math.Round(temperaturaCelsjusz, 2)}")
+            if (temperaturaCelsjusz < 0)
+            {
+                Console.WriteLine("uwazaj temperatura ponizej zera");
+            }
+        }
+    }
+
+}
