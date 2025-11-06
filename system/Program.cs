ad44829e40f8eb9cfed7092ac1fe97eb36b329bf
dawidskalka <jandrygasiewicz5@gmail.com>
2025-11-06 10:02:06 +0100

    system

---
+++ system/Program.cs
@@ -0,0 +1,31 @@
+﻿string loginPoprawny = "admin";
+string hasloPoprawne = "pass123";
+
+string loginUser = "";
+string hasloUser = "";
+int i = 3;
+while (i > 0)
+{
+
+    Console.Write("Podaj login: ");
+    loginUser = Console.ReadLine();
+    Console.Write("Podaj hasło: ");
+    hasloUser = Console.ReadLine();
+
+    if (loginUser == loginPoprawny && hasloUser == hasloPoprawne)
+    {
+        Console.WriteLine("Zalogowano pomyślnie.");
+        break;
+    }
+    else
+    {
+        Console.WriteLine($"Niepoprawny login lub hasło. Pozostaly {i - 1} proby");
+    }
+
+    i--;
+    if (i == 0)
+    {
+        Console.WriteLine("Konto zablokowane.");
+        break;
+    }
+}
