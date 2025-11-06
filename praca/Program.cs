6669d913eab937794ad9aacb54540c4e968d7e77
dawidskalka <jandrygasiewicz5@gmail.com>
2025-11-06 09:55:23 +0100

    liczbazadan

---
+++ praca/Program.cs
@@ -0,0 +1,22 @@
+﻿string liczbaZadan = Console.ReadLine();
+int liczba = int.Parse(liczbaZadan);
+while (liczba < 1 || liczba > 20)
+{
+    Console.WriteLine("Liczba zadań musi być z zakresu 1-20, wpisz ponownie: ");
+    liczbaZadan = Console.ReadLine();
+    liczba = int.Parse(liczbaZadan);
+}
+int j = 0;
+for (int i = 0; i <= liczba; i++)
+{
+    j = i + 1;
+    if (j % 2 == 0)
+    {
+        Console.WriteLine($"Zadanie {j} - Priorytet normalny");
+    }
+    else
+    {
+        Console.WriteLine($"Zadanie {j} - Priorytet wysoki");
+    }
+
+}
